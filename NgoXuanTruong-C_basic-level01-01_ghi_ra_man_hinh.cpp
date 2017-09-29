cae9f531ddc41cc643577282072a8c1b765fe182
NgoXuanTruong <32124494+NgoXuanTruong@users.noreply.github.com>
2017-09-29 23:30:56 +0700

    Delete NgoXuanTruong-C_basic-level01-01_ghi_ra_man_hinh.cpp

--- NgoXuanTruong-C_basic-level01-01_ghi_ra_man_hinh.cpp
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>;
-#include <conio.h>;
-main ()
-{
-	printf("\t \t \t \t CHUC MUNG! \n \t \t Gui den dong chi X,Y,Z.Chuc mung dong chi \n \t \t da rot tot nghiep va gia nhap quan ngu. ^^ ");
-}
